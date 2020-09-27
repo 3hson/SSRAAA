f7f6f52c19916a99df01def2b69833990f66c8ea
ehsan <3hson>
2020-09-27 09:32:50 +0330

    feat: add api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,55 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+const API_URl = 'localhost:3000/api';
+
+const login = async (email, password) => {
+  try {
+    const response = await axios.post(
+      `${API_URl}/users/login`,
+      JSON.stringify({ user: { email, password } }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const register  = async (username, email, password) => {
+  try {
+    const response = await axios.post(
+      `${API_URl}/users`,
+      JSON.stringify({ user: { username, email, password } }),
+      {
+        headers: {
+          'Content-Type': 'applicatizon/json',
+        },
+      }
+    );
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+const user = async () => {
+  const user = window.localStorage.getItem('user');
+  const token = user?.token;
+  try {
+    const response = await axios.get('/user', {
+      headers: {
+        Authorization: `Token ${encodeURIComponent(token)}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error.response;
+  }
+};
+
+export default {user, login, register};
